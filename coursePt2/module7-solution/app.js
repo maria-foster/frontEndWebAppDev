821a5d46234d472c6b109515bcaac67776d30f3c
Maria Foster <maria-foster@users.noreply.github.com>
2024-10-22 22:25:24 -0400

    DONE AND DONE

---
+++ coursePt2/module7-solution/app.js
@@ -0,0 +1,23 @@
+var app = angular.module("ShoppingListCheckOff", [])
+    .service('ShoppingListCheckOffService', function () {
+        this.toBuy = [ {name: "cookies", quantity: 10, pricePerItem: 1 }, {name: "pies", quantity: 10, pricePerItem: 1 }, {name: "doritos", quantity: 10, pricePerItem: 1 }, {name: "chickens", quantity: 10, pricePerItem: 1 }]
+        this.bought = []
+        // Service logic here
+        this.buy = function (itemBought) {
+            this.bought.push(itemBought);
+            index = this.toBuy.indexOf(itemBought)
+            this.toBuy.splice(index, 1)
+            console.log(this.toBuy)
+        }
+    })
+
+app.controller('ToBuyController', function ($scope, ShoppingListCheckOffService) { 
+    $scope.ShoppingListCheckOffService= ShoppingListCheckOffService;
+    $scope.toBuy = ShoppingListCheckOffService.toBuy
+
+})
+app.controller('AlreadyBoughtController', function ($scope, ShoppingListCheckOffService) {
+    $scope.ShoppingListCheckOffService= ShoppingListCheckOffService;
+    $scope.bought = ShoppingListCheckOffService.bought
+
+ })
