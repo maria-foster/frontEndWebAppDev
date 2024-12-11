80925a0fc64362463fa57fe72f9630a433c6e0fe
Maria Foster <maria-foster@users.noreply.github.com>
2024-12-10 20:38:23 -0500

    Adding test

---
+++ coursePt2/module10-solution/src/public/login/login.controller.spec-v2.js
@@ -0,0 +1,28 @@
+describe("Spec v2: Login Controller", function() {
+
+    beforeEach(function () {
+  
+      module('ShoppingListApp');
+    });
+  
+    var $controller;
+    var LoginController;
+  
+    beforeEach(inject(function (_$controller_) {
+      $controller = _$controller_;
+      LoginController =
+        $controller('LoginController');
+        window.localStorage.setItem('menu_item', JSON.stringify({"test" : "test"}))
+        window.localStorage.setItem('category', JSON.stringify({"test" : "test"}))
+        window.localStorage.setItem('user_info', JSON.stringify({"test" : "test"}))
+
+    }));
+  
+    it("should change fetch data from local storage", function() {
+      shoppingListController.fetchData();
+      expect(shoppingListController.menuItem).not.toBe(null);
+      expect(shoppingListController.category).not.toBe(null);
+      expect(shoppingListController.user).not.toBe(null);
+    });
+  
+  });
